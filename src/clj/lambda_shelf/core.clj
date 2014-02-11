cf41dbbae1203b2b14537d91e09b1ea1d3f81ea9
kordano <konrad.kuehne@rocketmail.com>
2014-02-11 17:45:58 +0100

    basic websocket server and production ring server

--- src/clj/lambda_shelf/core.clj
+++ src/clj/lambda_shelf/core.clj
@@ -1,6 +1,56 @@
-(ns lambda-shelf.core)
+(ns lambda-shelf.core
+  (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
+            [net.cgrand.enlive-html :as enlive]
+            [compojure.route :refer (resources)]
+            [compojure.core :refer (GET defroutes)]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [clojure.java.io :as io]
+            [org.httpkit.server :refer [with-channel on-close on-receive run-server send!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn destructure-request [{type :type data :data }]
+  (case type
+    "greeting" {:type "greeting" :data "Hail to the LAMBDA!"}
+    "DEFAULT"))
+
+
+                                        ; websocket server
+(defn handler [request]
+  (with-channel request channel
+    (on-close channel (fn [status] (println "channel closed: " status)))
+    (on-receive channel (fn [data]
+                          (do
+                            (println (str "data received: " (str (read-string data))))
+                            (send! channel (str (destructure-request (read-string data)))))))))
+
+
+(defn start-ws-server [port]
+  (run-server handler {:port port}))
+
+
+                                        ; ring server, only for production
+(enlive/deftemplate page
+  (io/resource "public/index.html")
+  []
+  [:body] (enlive/append
+            (enlive/html [:script (browser-connected-repl-js)])))
+
+(defroutes site
+  (resources "/")
+  (GET "/*" req (page)))
+
+(defn run
+  []
+  (defonce ^:private server
+    (ring.adapter.jetty/run-jetty #'site {:port 8080 :join? false}))
+  server)
+
+(defn -main
+  [& args]
+  (println "Starting ring server")
+  (run)
+  (println "Starting websocket server")
+  (start-ws-server 9090))
+
+#_(run)
+#_(start-ws-server 9090)
