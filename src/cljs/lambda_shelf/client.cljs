b0c1300b0ae1ff6b5efcded04aaf9629d4536fc2
kordano <konrad.kuehne@rocketmail.com>
2014-02-11 17:52:00 +0100

    very early bookmark view

---
+++ src/cljs/lambda_shelf/client.cljs
@@ -0,0 +1,79 @@
+(ns lambda-shelf.client
+  (:require [hiccups.runtime :as hiccupsrt]
+            [clojure.browser.repl]
+            [cljs.core.async :refer [put! chan <!]]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true])
+  (:require-macros [hiccups.core :as hiccups]
+                   [cljs.core.async.macros :refer [go]]))
+
+;; fire up repl
+#_(do
+    (def repl-env (reset! cemerick.austin.repls/browser-repl-env
+                         (cemerick.austin/repl-env)))
+    (cemerick.austin.repls/cljs-repl repl-env))
+
+
+(def app-state (atom {:bookmarks
+                      [{:title "the master himself" :url "https://github.com/kordano"}
+                       {:title "nomads blog" :url "http://functional-nomads.github.io"}
+                       {:title "solar raspberry" :url "http://www.instructables.com/id/Solar-Powered-Raspberry-Pi/?ALLSTEPS"}]}))
+
+
+(defn handle-url-change [e owner {:keys [url-text]}]
+  (om/set-state! owner :url-text (.. e -target -value)))
+
+(defn handle-title-change [e owner {:keys [title-text]}]
+  (om/set-state! owner :title-text (.. e -target -value)))
+
+(defn add-bookmark [app owner]
+  (let [new-title (.-value (om/get-node owner "new-title"))
+        new-url (.-value (om/get-node owner "new-url"))]
+    (om/transact! app :bookmarks conj {:title new-title :url new-url})
+    (om/set-state! owner :url-text "")
+    (om/set-state! owner :title-text "")))
+
+
+(defn bookmark-view [{:keys [title url] :as bookmark} owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/li nil
+              (dom/span nil (dom/a #js {:href url} title))
+              (dom/button #js {:onClick (fn [e] (put! delete @bookmark))} "Delete")))))
+
+
+(defn bookmarks-view [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)
+       :url-text ""
+       :title-text ""})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+              (let [bookmark (<! delete)]
+                (om/transact! app :bookmarks
+                              (fn [xs] (into [] (remove #(= bookmark %) xs))))
+                (recur))))))
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+               (dom/h2 nil "Bookmarks")
+               (dom/div nil
+                        (dom/span nil "URL"
+                                  (dom/input #js {:type "text" :ref "new-url" :value (:url-text state) :onChange #(handle-url-change % owner state)}))
+                        (dom/span nil "TITLE"
+                                  (dom/input #js {:type "text" :ref "new-title" :value (:title-text state) :onChange #(handle-title-change % owner state)}))
+
+                        (dom/button #js {:onClick #(add-bookmark app owner)} "Add bookmark"))
+               (apply dom/ul nil
+                      (om/build-all bookmark-view (:bookmarks app) {:init-state {:delete (:delete state)}}))))))
+
+
+(om/root
+  app-state
+  bookmarks-view
+  (. js/document (getElementById "bookmarks")))
