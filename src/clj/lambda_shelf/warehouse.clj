17eb1ca3175474f105b47d44e99c45eed2ed90d1
kordano <konrad.kuehne@rocketmail.com>
2014-03-12 19:25:14 +0100

    handle couchdb server address

--- src/clj/lambda_shelf/warehouse.clj
+++ src/clj/lambda_shelf/warehouse.clj
@@ -2,36 +2,46 @@
   (:refer-clojure :exclude [assoc! conj! dissoc! ==])
   (:require [clojure.core :as core]
             [lambda-shelf.database :as old]
+            [com.ashafa.clutch.utils :as utils]
             [com.ashafa.clutch :as cdb :refer [with-db get-database get-document put-document update-document all-documents]]))
 
 (defn now [] (new java.util.Date))
 
+
+(def host (or (System/getenv "DB_PORT_5984_TCP_ADDR") "localhost"))
+
+
+(defn database-url [database]
+  (utils/url (utils/url (str "http://" host ":5984")) database))
+
 (defn init-db []
-  (cdb/get-database "bookmark"))
+  (get-database (database-url "bookmark")))
 
 
 (defn insert-bookmark [{:keys [comment] :as bookmark}]
-  (let [entry (dissoc bookmark :comment)]
-    (put-document
-     "bookmark"
-     (assoc entry :votes 0 :date (now) :comments (if (= comment "") [] [comment])))))
+  (with-db (database-url "bookmark")
+    (let [entry (dissoc bookmark :comment)]
+      (put-document
+       (assoc entry :votes 0 :date (now) :comments (if (= comment "") [] [comment]))))))
 
 
 (defn vote-bookmark [{:keys [_id upvote]}]
-  (with-db "bookmark"
+  (with-db (database-url "bookmark")
       (update-document (get-document _id) update-in [:votes] inc)))
 
 
 (defn comment-bookmark [{:keys [_id comment]}]
-  (with-db "bookmark"
+  (with-db (database-url "bookmark")
     (update-document (get-document _id) update-in [:comments] #(conj % comment))))
 
 
 (defn get-all-bookmarks []
-  (let [ids (map #(:id %) (all-documents "bookmark"))]
-    (mapv #(dissoc (get-document "bookmark" %) :_rev) ids)))
+  (with-db (database-url "bookmark")
+    (let [ids (map #(:id %) (all-documents))]
+      (mapv #(dissoc (get-document %) :_rev) ids))))
 
 
 ;; --- testing vars ---
 
 #_(init-db)
+(println (get-all-bookmarks))
