f552205902b7aa993f26de551b96b7d70fdfd980
kordano <konrad.kuehne@rocketmail.com>
2014-03-15 15:51:36 +0100

    purge sql dependencies

--- project.clj
+++ project.clj
@@ -14,8 +14,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/java.jdbc "0.3.3"]
-                 [postgresql "9.1-901-1.jdbc4"]
                  [ring "1.2.1"]
                  [com.ashafa/clutch "0.4.0-RC1"]
                  [enlive "1.1.5"]
