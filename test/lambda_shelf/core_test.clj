3bff9a5eb81fb370edc88e99a0f1807b97ef8d4e
kordano <konrad.kuehne@rocketmail.com>
2014-03-25 20:47:59 +0100

    add websocket dispatch tests

--- test/lambda_shelf/core_test.clj
+++ test/lambda_shelf/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [lambda-shelf.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+
+(deftest test-bookmark-dispatch
+  (testing "Correct dispatch behaviour"
+    (let [fetch-url-title-request {:type :fetch-title :data {:url "http://www.youtube.com/watch?v=z5rRZdiu1UE-"}}]
+      (is (= {:title "Beastie Boys - Sabotage - YouTube"} (dispatch-bookmark test-request)))
+      (is (= "DEFAULT" (dispatch-bookmark {}))))))
