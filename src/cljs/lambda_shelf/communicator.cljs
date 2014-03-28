6515aa5c00a15a116858285cd67c7741b611ad81
kordano <konrad.kuehne@rocketmail.com>
2014-03-28 18:21:16 +0100

    add websocket connection

--- src/cljs/lambda_shelf/communicator.cljs
+++ src/cljs/lambda_shelf/communicator.cljs
@@ -4,6 +4,7 @@
             [cljs.core.async :as async :refer [chan close! put!]])
   (:require-macros [cljs.core.async.macros :refer [go alt!]]))
 
+(def websocket* (atom nil))
 
 (defn GET [url]
   (let [ch (chan 1)]
@@ -35,3 +36,30 @@
    (-> (POST url (.toString payload))
        <!
        read-string)))
+
+
+;; --- WEBSOCKET CONNECTION ---
+
+(defn send! [data]
+  (.send @websocket* (str data)))
+
+
+(defn- take-all! [raw-message]
+  (let [message (read-string raw-message)]
+    (.log js/console (str "data received: " message))))
+
+
+(defn client-connect! []
+  (.log js/console "establishing websocket ...")
+  (reset! websocket* (js/WebSocket. "ws://localhost:8080/bookmark/ws"))
+  (doall
+   (map #(aset @websocket* (first %) (second %))
+        [["onopen" (fn [] (do
+                           (.log js/console "channel opened")
+                           (.send @websocket* {:topic :greeting :data []})))]
+         ["onclose" (fn [] (.log js/console "channel closed"))]
+         ["onerror" (fn [e] (.log js/console (str "ERROR:" e)))]
+         ["onmessage" (fn [m]
+                        (let [data (.-data m)]
+                          (take-all! data)))]]))
+  (.log js/console "websocket loaded."))
