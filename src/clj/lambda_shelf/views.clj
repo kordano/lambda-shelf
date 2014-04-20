394d631f257ffa1632c8d31949ab15ad55e105de
kordano <konrad.kuehne@rocketmail.com>
2014-04-20 18:31:46 +0200

    add registration view

--- src/clj/lambda_shelf/views.clj
+++ src/clj/lambda_shelf/views.clj
@@ -2,6 +2,7 @@
   (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
             [net.cgrand.enlive-html :refer [deftemplate after html append]]
             [cemerick.friend :as friend]
+            [lambda-shelf.quotes :as quotes]
             [clojure.java.io :as io]))
 
 
@@ -21,7 +22,7 @@
        [:a.navbar-brand {:href "#"}
         "The Shelf"]]
       [:div.collapse.navbar-collapse
-       [:p.navbar-text "Signed in as " (-> req friend/identity :current)]
+       [:p#current-user-text.navbar-text (-> req friend/identity :current)]
        [:ul.nav.navbar-nav.navbar-right
         [:li
          [:a {:href "#"
@@ -41,6 +42,9 @@
               :data-placement "bottom"
               :title "Logout"}
           [:span.glyphicon.glyphicon-off]]]]]]]
+    [:div#quotes.container
+     [:div.well.well-sm
+      (quotes/random-quote)]]
     [:div#main.container]
     [:script {:src "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"}]
     [:script {:src "http://fb.me/react-0.9.0.min.js"}]
@@ -48,6 +52,55 @@
     [:script {:src "js/main.js"}]
     [:script (browser-connected-repl-js)])))
 
+
+(deftemplate registration
+  (io/resource "public/index.html")
+  []
+  [:body]
+  (append
+   (html
+    [:div.col-md-6.col-md-offset-3.col-sm-8.col-sm-offset-2
+     [:div#register-panel.panel.panel-primary
+      [:div.panel-heading
+       [:h3.panel-title "Registration"]]
+      [:div.panel-body
+       [:div#input-form {:role "form"}
+        [:form {:method "POST" :action "register"}
+
+         [:div.form-group
+          [:input.form-control
+           {:type "text"
+            :name "email"
+            :autocomplete "off"
+            :placeholder "e-mail"}]]
+
+         [:div.form-group
+          [:input.form-control
+           {:type "text"
+            :name "email-check"
+            :autocomplete "off"
+            :placeholder "Repeat e-mail"}]]
+
+         [:div.form-group
+          [:input.form-control
+           {:type "password"
+            :name "password"
+            :autocomplete "off"
+            :placeholder "Password"}]]
+
+         [:br]
+
+         [:input.btn.btn-primary.btn
+          {:type "submit"
+           :value "Register"}]]]]]])))
+
+(deftemplate register
+  (io/resource "public/index.html")
+  [req]
+  [:body]
+  (append
+   (html
+    )))
 
 (deftemplate login
   (io/resource "public/index.html")
@@ -78,4 +131,8 @@
 
          [:input.btn.btn-primary.btn-block
           {:type "submit"
-           :value "Login"}]]]]]])))
+           :value "Login"}]
+
+         [:br]
+
+         [:small "Not a member yet? Sign up " [:a {:href "registration"} "here"] "!"]]]]]])))
