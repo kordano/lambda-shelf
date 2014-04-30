de6f13d0dd0a2dc3fbce2612ac27d6522b0c5053
kordano <konrad.kuehne@rocketmail.com>
2014-04-30 17:45:06 +0200

    remove user content completely

--- src/cljs/lambda_shelf/core.cljs
+++ src/cljs/lambda_shelf/core.cljs
@@ -22,57 +22,6 @@
 
 (def app-state (atom {:bookmarks nil}))
 
-
-(def uri (goog.Uri. js/document.URL))
-
-(def ssl? (= (.getScheme uri) "https"))
-
-
-#_(defn user-list-view
-  "Side view for current registered users"
-  [app owner]
-  (reify
-    om/IInitState
-    {:ws-in (chan)
-     :ws-out (chan)}
-    om/IWillMount
-    (will-mount [_]
-      (go
-        (let [{:keys [in out websocket]}
-               (<! (connect!
-                    (str (if ssl? "wss://" "ws://")
-                         (.getDomain uri)
-                         ":"
-                         (.getPort uri)
-                         "/bookmark/ws")))]
-          (om/set-state! owner :ws-in in)
-          (om/set-state! owner :ws-out out)
-          (>! in {:topic :get-all-users :data []})
-          (let [nval (<! out)]
-            (om/transact!
-             app
-             :users
-             (fn [_] nval))))))
-    om/IRender
-    (render [_]
-      (html
-       [:div
-        [:h4 "The Collective"]
-        [:ul.list-group
-         (map
-          #(vec [:li.list-group-item.user-list-item
-                 [:p [:a {:href "#"
-                          :title "Add friend"}
-                      [:span.glyphicon.glyphicon-user]]
-                  %]])
-          (:users app))]]))))
-
-
-#_(om/root
- user-list-view
- app-state
- {:target (. js/document (getElementById "user-list-container"))})
-
 (om/root
  bookmarks-view
  app-state
